e109b0ae32a91009ba0fe34e87620316e4c6e836
David Beckingsale <davidbeckingsale@gmail.com>
2024-11-07 09:51:24 -0800

    Add CMake export (#31)

--- scripts/setup-tioga.sh
+++ scripts/setup-tioga.sh
@@ -7,6 +7,7 @@ cmake .. \
 -DLLVM_INSTALL_DIR=/opt/rocm-5.7.1/llvm \
 -DLLVM_VERSION=17 \
 -DCMAKE_C_COMPILER=amdclang -DCMAKE_CXX_COMPILER=amdclang++ -DENABLE_HIP=on \
+-DCMAKE_INSTALL_PREFIX=../install-tioga \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=on
 
 popd
